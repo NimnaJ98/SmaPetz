ba60b8f61923e965bdd2af148b82204f221bd2eb
Nimna <e1846608@bit.mrt.ac.lk>
2022-02-07 13:05:37 +0530

    created apps for posts and profiles, template folders and static folders for apps

--- SmaPetz/SmaPetz/settings.py
+++ SmaPetz/SmaPetz/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #resgister newly created apps
+    'posts',
+    'profiles',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'SmaPetz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/ 'static',
+    BASE_DIR/ 'posts' / 'static',
+    BASE_DIR/ 'profiles' / 'static',
+    ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/ 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ SmaPetz/posts/urls.py
@@ -0,0 +1,9 @@
+from django import views
+from django.urls import path
+from .views import home_view
+
+app_name ='posts'
+
+urlpatterns = [
+    path('', home_view, name='home-view'),
+]
