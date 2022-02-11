96d7763233fbed31252b3f8ee7e88ff947ee0023
Nimna <e1846608@bit.mrt.ac.lk>
2022-02-11 10:04:38 +0530

    Passed the query set in Post Model to the template

--- SmaPetz/posts/views.py
+++ SmaPetz/posts/views.py
@@ -1,6 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'posts/main.html', {'hello': 'hello world'})
+    #a query set to get all the posts
+    qs = Post.objects.all()
+    #pass the query set to the template
+    context = {
+        'hello':'hello world',
+        'qs':qs,
+    }
+    return render(request, 'posts/main.html', context)
