8e2efb0297dfbabdec9eca6ee573b3413dc3c50c
Nimna <e1846608@bit.mrt.ac.lk>
2022-02-15 11:15:30 +0530

    used javaScript to get all the posts and show them in the main template

--- SmaPetz/posts/urls.py
+++ SmaPetz/posts/urls.py
@@ -1,9 +1,13 @@
 from django import views
 from django.urls import path
-from .views import home_view
+from .views import home_view, post_view
 
 app_name ='posts'
 
 urlpatterns = [
     path('', home_view, name='home-view'),
+    
+
+    #endpoints
+    path('posts-jason/', post_view, name='posts-view-jason'),
 ]
--- SmaPetz/posts/views.py
+++ SmaPetz/posts/views.py
@@ -1,6 +1,8 @@
 from multiprocessing import context
 from django.shortcuts import render
 from .models import Post
+from django.http import JsonResponse
+from django.core import serializers
 
 # Create your views here.
 
@@ -13,3 +15,8 @@ def home_view(request):
         'qs':qs,
     }
     return render(request, 'posts/main.html', context)
+
+def post_view(request):
+    qs = Post.objects.all()
+    data = serializers.serialize('json', qs)
+    return JsonResponse({'data':data})
